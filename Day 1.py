666d27743b7744ca1a53c390a50834b99cfc98fc
ashish2024 <vtu17717@veltech.edu.in>
2026-08-19 18:49:42 +0530

    Added Python practice files

---
+++ Day 1.py
@@ -0,0 +1,27 @@
+name = "Ashish"
+age = 24
+salary = 50000
+
+# data types
+name = "Ashish"      # String
+age = 24             # Integer
+salary = 50000.50    # Float
+is_active = True     # Boolean
+
+
+print(type(name))
+print(type(age))
+
+
+
+name = input("Enter Name: ")
+print("Hello", name)
+
+
+name = input("Enter Name: ")
+age = int(input("Enter Age: "))
+city = input("Enter City: ")
+
+print("Name =", name)
+print("Age =", age)
+print("City =", city)
